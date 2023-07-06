273858a56ddd4ba733956a50f23257e794f8e1bf
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-07-06 13:50:45 -0400

    initial files

---
+++ app.js
@@ -0,0 +1,23 @@
+// class Person {
+
+//     name = 'Bertha';
+    
+//     constructor() {
+//         this.age = 20
+//     };
+
+//     greet() {
+//        console.log(`Well hello ${this.name}. I see you turned ${this.age} today.`)
+//     }
+// }
+
+function Person () {
+    this.age = 21;
+    this.name = 'Simon';
+    this.greet = function() {
+        console.log(`Well hello ${this.name}. I see you turned ${this.age} today.`) 
+    }
+}
+
+const person = new Person()
+person.greet()
