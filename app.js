c15df6fa155b55a90b21a15eea7f4ff327f98a26
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-07-08 15:33:07 -0400

    used object prototype to add methods to function constructors

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-// class Person {
+// class AgedPerson {
+//     printAge() {
+//         console.log(this.age)
+//     }
+// }
 
+// class Person extends AgedPerson{
 //     name = 'Bertha';
     
 //     constructor() {
+//         super()
 //         this.age = 20
 //     };
 
@@ -19,13 +25,27 @@ function Person () {
     }
 }
 
-Person.prototype = { // this is the way a child object extends it's parent
-    printAge() {
-        console.log(this.age)
-    }
+// Person.prototype = { // this is the same as a child object using extends
+//     printAge() {
+//         console.log(this.age)
+//     }
+// }
+
+Person.describe = function() {
+    console.log('Creating persons')
 }
+
+Person.prototype.printAge = function () {
+    console.log(this.age)
+}
+
+console.dir(Person)
+
 
 const person = new Person() 
 person.greet()
 person.printAge()
-// console.log(person.__proto__)
+console.log(person)
+console.log(person.toString())
+const person2 = new person.__proto__.constructor()
+console.log(Object)
