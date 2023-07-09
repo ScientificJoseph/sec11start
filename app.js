fdc8329d6924787d743c58eed9367817226b8455
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-07-09 13:04:19 -0400

    deep dive into constructor functions and prototypes

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
-// class AgedPerson {
-//     printAge() {
-//         console.log(this.age)
-//     }
-// }
-
-// class Person extends AgedPerson{
-//     name = 'Bertha';
-    
-//     constructor() {
-//         super()
-//         this.age = 20
-//     };
-
-//     greet() {
-//        console.log(`Well hello ${this.name}. I see you turned ${this.age} today.`)
-//     }
-// }
-
-function Person () {
-    this.age = 21;
-    this.name = 'Simon';
-    this.greet = function() {
-        console.log(`Well hello ${this.name}. I see you turned ${this.age} today.`) 
+class AgedPerson {
+    printAge() {
+        console.log(this.age)
     }
 }
+
+// class Person extends AgedPerson{
+    class Person {
+    name = 'Bertha';
+    
+    constructor() {
+        // super()
+        this.age = 20
+    };
+
+    // greet = () => {
+    //    console.log(`Well hello ${this.name}. I see you turned ${this.age} today.`)
+    // }
+    greet() {
+        console.log(`Well hello ${this.name}. I see you turned ${this.age} today.`)
+     }
+}
+
+// function Person () {
+//     this.age = 21;
+//     this.name = 'Simon';
+//     // this.greet = function() {
+//     //     console.log(`Well hello ${this.name}. I see you turned ${this.age} today.`) 
+//     // }
+// }
+// Person.prototype.greet = function() {
+//     console.log(`Well hello ${this.name}. I see you turned ${this.age} today.`) 
+// }
+
+// Person.describe = function() {
+//     console.log('Creating persons')
+// }
 
 // Person.prototype = { // this is the same as a child object using extends
 //     printAge() {
@@ -31,21 +42,25 @@ function Person () {
 //     }
 // }
 
-Person.describe = function() {
-    console.log('Creating persons')
-}
 
-Person.prototype.printAge = function () {
-    console.log(this.age)
-}
+// Person.prototype.printAge = function () {
+//     console.log(this.age)
+// }
 
-console.dir(Person)
+// console.dir(Person)
 
 
-const person = new Person() 
+// const person = new Person() 
+// person.greet()
+// person.printAge()
+// console.log(person.length)
+// console.log(person.toString())
+// const person2 = new person.__proto__.constructor()
+// console.dir(Object.prototype.__proto__)
+
+const person = new Person();
+const person2 = new Person();
 person.greet()
-person.printAge()
-console.log(person)
-console.log(person.toString())
-const person2 = new person.__proto__.constructor()
-console.log(Object)
+console.log(person.__proto__ == person2.__proto__)
+const btn = document.getElementById('btn')
+btn.addEventListener('click', person.greet.bind(person))
